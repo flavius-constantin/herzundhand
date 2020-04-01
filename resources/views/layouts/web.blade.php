494c13e16bfa14f3cabc94df234be50c954c1af4
Flavius "Cookie" Constantin <me@flavius-constantin.de>
2020-04-01 20:09:35 +0200

    ???

--- resources/views/layouts/web.blade.php
+++ resources/views/layouts/web.blade.php
@@ -189,6 +189,7 @@
     </footer>
 
     <!-- Scripts -->
+    <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.0.1/dist/alpine.js" defer></script>
     <script src="{{ mix('js/app.js') }}"></script>
     @stack('scripts')
 </body>
