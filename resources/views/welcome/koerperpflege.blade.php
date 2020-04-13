5e8f26db3f785ae39eccb5d6168325b0608fe26f
Flavius "Cookie" Constantin <me@flavius-constantin.de>
2020-04-13 15:20:56 +0200

    Welcome Page Refactoring
    Create separate Views for Categories

---
+++ resources/views/welcome/koerperpflege.blade.php
@@ -0,0 +1,52 @@
+<section class="px-6 md:px-0 py-10 bg-pattern">
+    <div class="container text-center">
+        <h3 class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-800 sm:text-4xl sm:leading-10">
+            Körperpflege
+        </h3>
+        <p class="mt-4 max-w-2xl text-xl leading-7 text-gray-600 lg:mx-auto">
+            Lorem ipsum dolor sit amet consect adipisicing elit. Possimus magnam voluptatum cupiditate veritatis in accusamus quisquam.
+        </p>
+        <div class="grid grid-cols-1 md:grid-cols-2 mt-8 col-gap-16">
+            <div class="flex items-center lg:ml-20">
+                <span class="w-10 h-10 text-base font-semibold flex justify-center items-center uppercase rounded-full text-green-600 bg-green-200 mr-3">
+                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8">
+                        <path d="M5 13l4 4L19 7"></path>
+                    </svg>
+                </span>
+                <p class="text-gray-700">
+                    Waschen (Teil- und Ganzkörperwäsche)
+                </p>
+            </div>
+            <div class="flex items-center">
+                <span class="w-10 h-10 text-base font-semibold flex justify-center items-center uppercase rounded-full text-green-600 bg-green-200 mr-3">
+                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8">
+                        <path d="M5 13l4 4L19 7"></path>
+                    </svg>
+                </span>
+                <p class="text-gray-700">
+                    Zahnpflege
+                </p>
+            </div>
+            <div class="flex items-center lg:ml-20">
+                <span class="w-10 h-10 text-base font-semibold flex justify-center items-center uppercase rounded-full text-green-600 bg-green-200 mr-3">
+                    <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-8 h-8">
+                        <path d="M5 13l4 4L19 7"></path>
+                    </svg>
+                </span>
+                <p class="text-gray-700">
+                    Wechseln von Inkontinenzmaterial
+                </p>
+            </div>
+            <div class="mt-6 text-left">
+                <a href="#" class="group py-3 px-4 border border-transparent text-xs uppercase font-bold rounded-full text-green-600 hover:text-white bg-green-200 hover:bg-green-600 focus:outline-none focus:shadow-outline transition duration-150 ease-in-out">
+                    Weiterlesen
+                    <span class="hidden group-hover:inline-block">
+                        <svg fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" class="w-5 h-5 inline-flex -mt-1">
+                            <path d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122"></path>
+                        </svg>
+                    </span>
+                </a>
+            </div>
+        </div>
+    </div>
+</section>
