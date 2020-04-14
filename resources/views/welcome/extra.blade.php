d7c6de785a3ca5c6d6a097f38baf7bf6a5b2da72
Flavius "Cookie" Constantin <me@flavius-constantin.de>
2020-04-14 14:21:55 +0200

    Add some Extras

--- resources/views/welcome/extra.blade.php
+++ resources/views/welcome/extra.blade.php
@@ -1,53 +1,56 @@
 <section class="relative block bg-gray-900">
-    <!-- <div class="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20" style="height: 80px; transform: translateZ(0px);">
+    <div class="bottom-auto top-0 left-0 right-0 w-full absolute pointer-events-none overflow-hidden -mt-20" style="height: 80px; transform: translateZ(0px);">
         <svg class="absolute bottom-0 overflow-hidden" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" version="1.1" viewBox="0 0 2560 100" x="0" y="0">
             <polygon class="text-gray-900 fill-current" points="2560 0 2560 100 0 100"></polygon>
         </svg>
-    </div> -->
-    <div class="container mx-auto px-4 lg:pt-24 lg:pb-12">
+    </div>
+    <div class="container mx-auto px-4 py-12">
         <div class="flex flex-wrap text-center justify-center">
-            <div class="w-full lg:w-6/12 px-4">
-                <h2 class="text-4xl font-semibold text-white">Extras</h2>
+            <div class="w-full lg:w-8/12 px-4">
+                <h2 class="text-4xl font-semibold text-white">
+                    Extra
+                </h2>
                 <p class="text-lg leading-relaxed mt-4 mb-4 text-gray-500">
-                    Put the potentially record low maximum sea ice extent tihs year
-                    down to low ice. According to the National Oceanic and
-                    Atmospheric Administration, Ted, Scambos.
+                    Selbstverständlich ist das nicht alles, jedoch sind wir der Meinung, dass Sie folgendes nicht unbeachtet lassen sollen:
                 </p>
             </div>
         </div>
         <div class="flex flex-wrap mt-12 justify-center">
-            <div class="w-full lg:w-3/12 px-4 text-center">
+            <div class="w-full lg:w-4/12 px-4 text-center">
                 <div class="text-gray-900 p-3 w-12 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center">
-                    <i class="fas fa-medal text-xl"></i>
+                    <svg class="w-8 h-8" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24">
+                        <path d="M14.121 15.536c-1.171 1.952-3.07 1.952-4.242 0-1.172-1.953-1.172-5.119 0-7.072 1.171-1.952 3.07-1.952 4.242 0M8 10.5h4m-4 3h4m9-1.5a9 9 0 11-18 0 9 9 0 0118 0z"></path>
+                    </svg>
                 </div>
                 <h6 class="text-xl mt-5 font-semibold text-white">
-                    Excelent Services
+                    Kostentransparenz
                 </h6>
                 <p class="mt-2 mb-4 text-gray-500">
-                    Some quick example text to build on the card title and make up
-                    the bulk of the card's content.
+                    Abgesehen von einer Tagesgenauen Abrechnung, erhalten Sie von uns selbstverständlich totale Kostentransparenz. Bei uns sehen Sie genau <span class="underline font-bold">wie viel,</span> Sie <span class="underline font-bold">für was</span> bezahlen.
                 </p>
             </div>
-            <div class="w-full lg:w-3/12 px-4 text-center">
+            <div class="w-full lg:w-4/12 px-4 text-center">
                 <div class="text-gray-900 p-3 w-12 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center">
-                    <i class="fas fa-poll text-xl"></i>
+                    <svg class="w-8 h-8" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24">
+                        <path d="M13 10V3L4 14h7v7l9-11h-7z"></path>
+                    </svg>
                 </div>
                 <h5 class="text-xl mt-5 font-semibold text-white">
-                    Grow your market
+                    schnelle Vermittlung
                 </h5>
                 <p class="mt-2 mb-4 text-gray-500">
-                    Some quick example text to build on the card title and make up
-                    the bulk of the card's content.
+                    Trotz der Entfernung und den Grenzen die zwischen einem unserer Pflegeengel und dem Klienten liegen, gelingt es uns in der Regel, mit einem Auftrag <span class="underline font-bold">binnend 7 Tagen</span> zu beginnen.
                 </p>
             </div>
-            <div class="w-full lg:w-3/12 px-4 text-center">
+            <div class="w-full lg:w-4/12 px-4 text-center">
                 <div class="text-gray-900 p-3 w-12 h-12 shadow-lg rounded-full bg-white inline-flex items-center justify-center">
-                    <i class="fas fa-lightbulb text-xl"></i>
+                    <svg class="w-8 h-8" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24">
+                        <path d="M18.364 18.364A9 9 0 005.636 5.636m12.728 12.728A9 9 0 015.636 5.636m12.728 12.728L5.636 5.636"></path>
+                    </svg>
                 </div>
-                <h5 class="text-xl mt-5 font-semibold text-white">Launch time</h5>
+                <h5 class="text-xl mt-5 font-semibold text-white">keine Schwarzarbeit</h5>
                 <p class="mt-2 mb-4 text-gray-500">
-                    Some quick example text to build on the card title and make up
-                    the bulk of the card's content.
+                    Wir legen viel Wert auf die Sicherheit unserer Kunden, ebenso unserer Angestellten. Unsere Mitarbeiter sind <span class="underline font-bold">legal</span> angestellt, zahlen Steuern, Sozialleistungen und somit auch gesetzlich Krankenversichert!
                 </p>
             </div>
         </div>
