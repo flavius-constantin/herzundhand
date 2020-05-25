09c0f5eb09cf2d7fbf0b091858996e826232ca34
Flavius "Cookie" Constantin <me@flavius-constantin.de>
2020-05-25 16:18:58 +0200

    WIP: SEO

--- resources/views/includes/_seo.blade.php
+++ resources/views/includes/_seo.blade.php
@@ -1,15 +1,15 @@
-<meta name="description" content="lalalalala">
+<meta name="description" content="Das Modell der 24 Stunden Pflege zu Hause bietet Ihren hilfebedürftigen Angehörigen professionelle Unterstützung im Alltag und entlastet Sie von den vielfältigen und zeitintensiven Aufgaben, die eine professionelle Rundumbetreuung mit sich bringt. Erfahrene osteuropäische Betreuungs- und Pflegekräfte aus Rumänien kümmern sich fürsorglich um die Bedürfnisse Ihrer liebsten.">
 
 <!-- Schema.org markup for Google+ -->
 <meta itemprop="name" content="{{ config('app.name') }}">
-<meta itemprop="description" content="This is the page description">
+<meta itemprop="description" content="Wenn Pflegebedürftige Menschen rund um die Uhr im eigenen Zuhause betreut werden, nennt man dies „24 Stunden Betreuung“">
 <meta itemprop="image" content="{{ asset('logo.jpg') }}">
 
 <!-- Twitter Card data -->
 <meta name="twitter:card" content="product">
 <!-- <meta name="twitter:site" content="@publisher_handle"> -->
 <meta name="twitter:title" content="{{ config('app.name') }}">
-<meta name="twitter:description" content="Page description less than 200 characters">
+<meta name="twitter:description" content="Betreuungsbedürftigen Menschen ein würdevolles Leben im vertrauten Umfeld ermöglichen.">
 <meta name="twitter:creator" content="@flavius2105">
 <!-- Twitter summary card with large image must be at least 280x150px -->
 <meta name="twitter:image:src" content="{{ asset('logo.jpg') }}">
@@ -19,7 +19,7 @@
 <meta property="og:type" content="article" />
 <meta property="og:url" content="{{ config('app.url') }}" />
 <meta property="og:image" content="{{ asset('logo.jpg') }}" />
-<meta property="og:description" content="Description Here" />
+<meta property="og:description" content="Wenn Pflegebedürftige Menschen rund um die Uhr im eigenen Zuhause betreut werden, nennt man dies „24 Stunden Betreuung“" />
 <meta property="og:site_name" content="{{ config('app.name') }}" />
 
 <!-- <meta property="article:published_time" content="2013-09-17T05:59:00+01:00" />
